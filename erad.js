a022dd561762cd878f4d04930a15997152b116b4
Anton O <anton.osika@gmail.com>
2015-05-31 23:58:19 +0200

    2.0

--- erad.js
+++ erad.js
@@ -1,17 +1,25 @@
-var divs = document.getElementsByTagName("div"), item;
-var stream = 0, ticker = 0;
-for (var i = 0, len = divs.length; i < len; i++) {
-    if (ticker && stream) break;
-    item = divs[i];
-    if (item.id) {
-        if (!ticker && item.id.match("pagelet_ticker") != null) { 
-            item.remove();
-            ticker = 1;
-        }
-        else if (!stream && item.id.match("topnews_main_stream") != null) {
-            item.remove()
-            stream = 1;
+
+
+var tryRemove = function () {
+    var stream = false, ticker = false;
+    if (! ( stream || ticker)) {
+    var divs = document.getElementsByTagName("div"), item;
+
+    for (var i = 0, len = divs.length; i < len; i++) {
+
+        item = divs[i];
+        if (item.id) {
+            if (!ticker && item.id.match("pagelet_ticker") != null) { 
+                item.remove();
+                ticker = true;
+            }
+            else if (!stream && item.id.match("topnews_main_stream") != null) {
+                item.remove()
+                stream = true;
+            }
         }
     }
 }
+}
 
+setInterval(tryRemove, 100);
