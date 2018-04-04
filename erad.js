111ee8d2d9e0aa527da9db652a4e45210bf0f6ff
Mike Szyndel <hajder@users.noreply.github.com>
2018-04-04 12:27:52 +0200

    Refactor to use querySelectorAll and check window host

--- erad.js
+++ erad.js
@@ -1,25 +1,12 @@
 
 
 var tryRemove = function () {
-    var stream = false, ticker = false;
-    if (! ( stream || ticker)) {
-    var divs = document.getElementsByTagName("div"), item;
-
-    for (var i = 0, len = divs.length; i < len; i++) {
-
-        item = divs[i];
-        if (item.id) {
-            if (!ticker && item.id.match("pagelet_ticker") != null) { 
-                item.remove();
-                ticker = true;
-            }
-            else if (!stream && item.id.match("topnews_main_stream") != null) {
-                item.remove()
-                stream = true;
-            }
-        }
-    }
-}
+    document.querySelectorAll('div[id*=pagelet_ticker]').forEach(function(item) {
+        item.remove()
+    });
+    document.querySelectorAll('div[id*=topnews_main_stream]').forEach(function(item) {
+        item.remove()
+    });
 }
 
 if(window.location.host === 'www.facebook.com') {
